f9eb1b2ac21d6f4c4e731d25980235e33b8e1471
jazzssil <40033414+jazzssil@users.noreply.github.com>
2020-08-17 21:01:57 +0800

    使用scala做为server, java 作为客户端

--- master/src/main/scala/HelloServer.scala
+++ master/src/main/scala/HelloServer.scala
@@ -1,15 +1,17 @@
+import io.grpc.{ServerBuilder, ServerServiceDefinition}
 import io.grpc.examples.helloworld.{GreeterGrpc, Person, ProtoRequest, ProtoResponse}
 import io.grpc.stub.StreamObserver
 
 import scala.concurrent.Future
 
 object HelloServer extends GRPCServer{
-	class HelloService extends GreeterGrpc.GreeterImplBase{
+	object HelloService extends GreeterGrpc.GreeterImplBase{
 		override def receive(request: ProtoRequest, responseObserver: StreamObserver[ProtoResponse]): Unit = {
 			val name = request.getPerson match {
-				case Person => request.getPerson.getName
+				case p:Person => p.getName
 				case _ => "a"
 			}
+			println("p : " + name)
 			val response = ProtoResponse.newBuilder().setMessage(name).build()
 			responseObserver.onNext(response)
 			responseObserver.onCompleted()
@@ -17,6 +19,7 @@ object HelloServer extends GRPCServer{
 	}
 
 	def main(args: Array[String]): Unit = {
-		println("a")
+		val server = ServerBuilder.forPort(50052).addService(HelloService).build().start()
+		server.awaitTermination()
 	}
 }
